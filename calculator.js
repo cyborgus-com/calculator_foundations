9cf384b6725766ae2a018f2c5ed77bd35f0483c4
karolisurbonas <karolis.urbonas@gmail.com>
2024-12-04 19:06:57 -1000

    files created, basic css done, html basic view done, js query empty selectors ready

---
+++ calculator.js
@@ -0,0 +1,24 @@
+screen = document.querySelector("#screen");
+
+reset = document.querySelector("#");
+posneg = document.querySelector("#");
+percentage = document.querySelector("#");
+comma = document.querySelector("#");
+equals = document.querySelector("#");
+
+one = document.querySelector("#");
+two = document.querySelector("#");
+three = document.querySelector("#");
+four = document.querySelector("#");
+five = document.querySelector("#");
+six = document.querySelector("#");
+seven = document.querySelector("#");
+eight = document.querySelector("#");
+nine = document.querySelector("#");
+zero = document.querySelector("#");
+
+multiply = document.querySelector("#");
+divide = document.querySelector("#");
+minus = document.querySelector("#");
+plus = document.querySelector("#");
+
